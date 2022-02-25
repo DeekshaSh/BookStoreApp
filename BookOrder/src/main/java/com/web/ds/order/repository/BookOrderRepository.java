ff26deafc57ce33eb8dc877d2921ab9fec2c3056
DeekshaSh <deeksha.sh20@gmail.com>
2022-02-25 12:51:43 +0530

    Add files via upload

--- BookOrder/src/main/java/com/web/ds/order/repository/BookOrderRepository.java
+++ BookOrder/src/main/java/com/web/ds/order/repository/BookOrderRepository.java
@@ -1,9 +1,11 @@
 package com.web.ds.order.repository;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.web.ds.order.domain.BookOrder;
@@ -11,7 +13,7 @@ import com.web.ds.order.domain.BookOrder;
 @Repository
 public interface BookOrderRepository extends CrudRepository<BookOrder, String> {
 	
-	@Query("SELECT order FROM book_order order WHERE order.id = ?1")
-	Map<String, BookOrder> findBookById(String orderId);
+	//@Query("SELECT order FROM BookOrder order WHERE order.orderId =:orderId")
+	List<BookOrder> findByOrderId(@Param("orderId") String orderId);
 	
 }
